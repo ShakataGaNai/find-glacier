d91f116978740b553a78af76ab6fe0ba2856c931
Jon Davis <jon@snowulf.com>
2015-07-24 11:48:12 -0700

    Cleaning up, commenting.

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # find-glacier
-Just a quick script to find all the data stored in AWS Glacier
+A quick nodejs script to find all files stored in [AWS Glacier](http://aws.amazon.com/glacier/) (or optionally all files in S3).
 
 ## How to use
+* Copy config-example.json to config.json
 * Login to AWS console
 * Goto IAM
-* Create a new user, save the access key & secret
+* Create a new user, save the access key & secret to config.json
+ * NOTE: Do not change the region from us-east-1.
 * Attach the policy "AmazonS3ReadOnlyAccess" to the user
 * Install the dependencies: `$ npm install`
 * Run the app: `$ node app.js`
+* Change `var glacierOnly = true;` to `false` to list ALL S3 files.
+
+## License
+See LICENSE file
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Setup our key variables and load some modules
 var AWS = require('aws-sdk');
 var Q = require('q');
 AWS.config.loadFromPath("./config.json");
@@ -6,16 +7,19 @@ var s3x = new Array();
 s3x['us-east-1'] = new AWS.S3({endpoint: 'https://s3.amazonaws.com'});
 var allBucket = new Array();
 
-// Configure me!
+// **** Configure me! ****
 var debug = false;
 var glacierOnly = true;
-// End Configure me!
+// **** End Configure me! ****
 
+// **** Main code loop ****
  listTheBuckets()
   .then(getBucketLocale)
   .then(listAll)
+// **** End main loop ****
 
-// **************************
+
+// **** Functions start here ****
 function printMe(){
   // Function used for debug only
   var deferred = Q.defer();
@@ -25,38 +29,9 @@ function printMe(){
   return deferred.promise;
 }
 
-function getBucketLocale(){
-  loggit("getBucketLocale()");
-  var deferred = Q.defer();
-  var areWeThereYet = 0;
-  Object.keys(allBucket).forEach(function(name) {
-
-    areWeThereYet++;
-    s3.getBucketLocation({Bucket: name}, function(err3, data3) {
-      if(err3){loggit(err3);}
-
-      if(data3.LocationConstraint.length > 1 && !s3x[data3.LocationConstraint]){
-        s3x[data3.LocationConstraint] = new AWS.S3({endpoint: 'https://s3-'+data3.LocationConstraint+'.amazonaws.com'});
-        loggit("Opened new S3x " + data3.LocationConstraint);
-      }
-
-      if(data3.LocationConstraint.length > 1){
-        allBucket[name] = data3.LocationConstraint;
-      }else{
-        allBucket[name] = "us-east-1";
-      }
-      areWeThereYet--;
-      if(areWeThereYet == 0){
-        loggit("getBucketLocale()-resolve()");
-        deferred.resolve();
-      }
-    }); //End s3.getBucketLocation
-  }); //End forEach
-  loggit("leaving getBucketLocale()");
-  return deferred.promise;
-}
-
 function listTheBuckets(){
+    // Fetches a list of all S3 buckets.
+
     var deferred = Q.defer();
     loggit("listTheBuckets()");
     s3.listBuckets(function (err, data) {
@@ -73,11 +48,51 @@ function listTheBuckets(){
   return deferred.promise;
 }
 
-function listAll(){
-  loggit("listAll()");
+function getBucketLocale(){
+  // Takes the list of S3 buckets, figures out where they are
+  //   and opens a connection to those S3 end points.
+
+  loggit("getBucketLocale()");
   var deferred = Q.defer();
   var areWeThereYet = 0;
   Object.keys(allBucket).forEach(function(name) {
+
+    areWeThereYet++;
+    s3.getBucketLocation({Bucket: name}, function(err3, data3) {
+      if(err3){loggit(err3);}
+
+      if(data3.LocationConstraint.length > 1 && !s3x[data3.LocationConstraint]){
+        // Do we already have this S3 connection open? If not, open it.
+        s3x[data3.LocationConstraint] = new AWS.S3({endpoint: 'https://s3-'+data3.LocationConstraint+'.amazonaws.com'});
+        loggit("Opened new S3x " + data3.LocationConstraint);
+      }
+
+      if(data3.LocationConstraint.length > 1){
+        // Store the location with the name.
+        allBucket[name] = data3.LocationConstraint;
+      }else{
+        allBucket[name] = "us-east-1";
+      }
+      areWeThereYet--;
+      if(areWeThereYet == 0){
+        loggit("getBucketLocale()-resolve()");
+        deferred.resolve();
+      }
+    }); //End s3.getBucketLocation
+  }); //End forEach
+  loggit("leaving getBucketLocale()");
+  return deferred.promise;
+}
+
+function listAll(){
+  // Pull a list of every S3 "object" (file) in every bucket
+
+  loggit("listAll()");
+  var deferred = Q.defer();
+  var areWeThereYet = 0;
+
+  Object.keys(allBucket).forEach(function(name) {
+    // Iterate through our list of buckets
     areWeThereYet++;
     s3x[allBucket[name]].listObjects({Bucket: name}, function(err2, objdata) {
       if(err2){
@@ -86,8 +101,10 @@ function listAll(){
         if(objdata.Contents.length>0){
           for(var a=0;a<objdata.Contents.length;a++){
             if(glacierOnly && objdata.Contents[a].StorageClass == "GLACIER"){
+              //If we're in glacierOnly mode and is a glacier file - print
               console.log("s3://"+allBucket[name]+"/"+name+"/"+objdata.Contents[a].Key+" -- "+objdata.Contents[a].StorageClass);
             }else if(!glacierOnly){
+              //We're not in glacierOnly mode - print
               console.log("s3://"+allBucket[name]+"/"+name+"/"+objdata.Contents[a].Key+" -- "+objdata.Contents[a].StorageClass);
             }
           }
@@ -107,5 +124,6 @@ function listAll(){
 } //end function
 
 function loggit(message){
+  // Debugging ftw
   if(debug){console.log(message);}
 }
